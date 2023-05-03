f12fbc78dbd82f72553b8170c950adc220223b1f
Vincenzo FOTI <vincenzo.foti@outlook.it>
2023-05-03 17:38:22 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Controllers/MenuController.cs
@@ -0,0 +1,37 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizza = db.pizze.ToList<Pizza>();
+                return View(pizza);
+            }
+        }
+
+        public IActionResult Details(int id)
+        {
+            using PizzaContext db = new PizzaContext();
+            Pizza pizza = db.pizze.FirstOrDefault(m => m.Id == id);
+            return View("Show", pizza);
+        }
+
+
+
+        /*public IActionResult Add()
+        {
+            using PizzaContext db = new PizzaContext();
+            Pizza pizza = new Pizza();
+            db.Add(pizza);
+            db.SaveChanges();
+
+            return View();
+        }
+        */
+    }
+}
---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string? Image { get; set; }
+        public double Price { get; set; }
+
+        public Pizza()
+        {
+
+        }
+        public Pizza(string nome, string descrizione, string image, double price)
+        {
+            Nome = nome;
+            Descrizione = descrizione;
+            Image = image;
+            Price = price;
+        }
+    }
+}
